72bbe0811c7f9254e6ae82fb821d99475e404ee8
Nasruddin <webo.geeky@gmail.com>
2017-12-22 11:46:19 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/javatab/SpringAtomistApplication.java
@@ -0,0 +1,12 @@
+package com.javatab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAtomistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAtomistApplication.class, args);
+	}
+}
---
+++ src/test/java/com/javatab/SpringAtomistApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javatab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAtomistApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
